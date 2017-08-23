eae04b92f4b108c9894a4cc1fc33af1be5223fa3
Piero Divasto <pidivas@gap.com>
2017-08-23 18:35:20 -0300

    Added custom error

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const {graphqlExpress, graphiqlExpress} = require('apollo-server-express');
 const {authenticate} = require('./authentication');
 const schema = require('./schema');
 const builDataLoaders = require('./dataloaders');
+const formatError = require('./formatError');
 
 // 1
 const connectMongo = require('./mongo-connector');
@@ -12,7 +13,7 @@ const connectMongo = require('./mongo-connector');
 const start = async () => {
   // 3
   const mongo = await connectMongo();
-  var app = express();
+  let app = express();
 
   const buildOptions = async (req, res) => {
     const user = await authenticate(req, mongo.Users);
@@ -22,6 +23,7 @@ const start = async () => {
         mongo,
         user
       },
+      formatError,
       schema
     };
   };
